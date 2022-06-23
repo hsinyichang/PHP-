079718fd9b4413df101bd8dc1974bf557fe5b191
HsinYiChang <f1233772002@gmail.com>
2022-06-23 14:29:08 +0800

    建議使用PHP開啟檔案及讀取檔案內容的機制和流程

--- text-import.php
+++ text-import.php
@@ -8,6 +8,26 @@
  * 6.寫入資料庫
  * 7.結束檔案
  */
+include_once "base.php";
+//2022/0623/14:22講解
+if(isset($_FILES['file']) && $_FILES['file']['error']==0){
+    echo $_FILES['file']['tmp_name'];
+    $file=fopen($_FILES['file']['tmp_name'],'r');
+    fgets($file);
+    while(!feof($file)){
+        $str=fgets($file);
+        $col=explode(',',$str);
+        dd($col);
+        exit();
+    }
+   
+    //$sql="insert into `upload` (`name`,`type`,`collections`) values('{$name}','{$type}','{$_POST['collections']}')";
+    //$pdo->exec($sql);
+
+}
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -22,7 +42,13 @@
 <body>
 <h1 class="header">文字檔案匯入練習</h1>
 <!---建立檔案上傳機制--->
+<form id="uploadForm" action="?" method="post" enctype="multipart/form-data">
+    <div>
+        選擇檔案:<input type="file" name="file">
+    </div>
 
+    <input type="submit" value="上傳">
+</form>
 
 
 <!----讀出匯入完成的資料----->
